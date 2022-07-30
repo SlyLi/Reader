a597f9d0e435732a4a50e2815df93a29fcd46418
SlyLi <slylicn@qq.com>
2022-07-31 02:08:56 +0800

    update

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
     'reader.apps.ReaderConfig',
     'django.contrib.admin',
     'django.contrib.auth',
